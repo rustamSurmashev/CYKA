8258eb4ebbefdde84d685fdb9eb87326c5f23454
rustamSurmashev <rustamsurmashev@gmail.com>
2019-04-11 00:03:21 +0300

    global changes

---
+++ src/js/modal.js
@@ -0,0 +1,18 @@
+let confirmButtonEl = document.getElementById("confirmButton");
+let modal = document.querySelector("#modal");
+let modalOverlay = document.querySelector("#modal-overlay");
+let closeButton = document.querySelector("#close-button");
+
+confirmButtonEl.addEventListener('click', () => {
+
+    //TODO: Валидация формы
+
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+});
+
+
+closeButton.addEventListener("click", function(){
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+});
