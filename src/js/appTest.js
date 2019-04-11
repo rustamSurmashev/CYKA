e9d08f34b478ed114d7a72fd380e62af2b53fe34
rustamSurmashev <rustamsurmashev@gmail.com>
2019-04-11 11:01:40 +0400

    some problems fixed

--- src/js/appTest.js
+++ src/js/appTest.js
@@ -1,4 +1,4 @@
-let donatEl = document.getElementById('donat');
+let donatEl = document.getElementById('donateButton');
 let newTestEl = document.getElementById('newTest');
 
 donatEl.addEventListener('click', ()=>{
@@ -7,60 +7,3 @@ donatEl.addEventListener('click', ()=>{
 newTestEl.addEventListener('click', ()=>{
     document.location.href = 'newTest.html';
 });
-
-let programmingEl = document.getElementById('programming');
-let physicsEl = document.getElementById('physics');
-let chimistryEl = document.getElementById('chimistry');
-let oilEl = document.getElementById('oil');
-let sportEl = document.getElementById('sport');
-let phylosophyEl = document.getElementById('phylosophy');
-let historyEl = document.getElementById('history');
-let mathEl = document.getElementById('math');
-let geographyEl = document.getElementById('geography');
-let medicineEl = document.getElementById('medicine');
-let journalismEl = document.getElementById('journalism');
-let literatureEl = document.getElementById('literature');
-let lowEl = document.getElementById('low');
-
-programmingEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-physicsEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-chimistryEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-oilEl.addEventListener('click', ()=>{
-    document.location.href = 'oilTest.html';
-});
-sportEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-phylosophyEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-historyEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-mathEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-geographyEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-medicineEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-journalismEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-literatureEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-lowEl.addEventListener('click', ()=>{
-    document.location.href = 'empty.html';
-});
-
-
-
