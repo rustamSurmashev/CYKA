428b709ed4e18950a9ea1fe3ecf2480af0d64f13
rustamSurmashev <rustamsurmashev@gmail.com>
2019-04-12 19:07:08 +0400

    empty background added

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,14 +4,10 @@ let modalOverlay = document.querySelector("#modal-overlay");
 let closeButton = document.querySelector("#close-button");
 
 confirmButtonEl.addEventListener('click', () => {
-
     //TODO: Валидация формы
-
     modal.classList.toggle("closed");
     modalOverlay.classList.toggle("closed");
 });
-
-
 closeButton.addEventListener("click", function(){
     modal.classList.toggle("closed");
     modalOverlay.classList.toggle("closed");
